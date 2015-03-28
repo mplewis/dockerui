d76a5be071250d04462ff40f3807eb0a7e56506c
Matthew Lewis <matt@mplewis.com>
2015-03-28 00:01:57 -0500

    Add jQuery to all modules

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack')
+
 module.exports = {
   entry: './app/app.js',
   output: {
@@ -5,6 +7,13 @@ module.exports = {
     filename: 'bundle.js'
   },
   devtool: 'eval-source-map',
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+    })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
