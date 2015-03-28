158deccfdd6393499f3b4c2fc6d38e77e0e2121c
Matthew Lewis <matt@mplewis.com>
2015-03-28 02:25:09 -0500

    Tidy up Gulpfile and get rid of race condition

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@ var webpack = require('gulp-webpack')
 var addsrc = require('gulp-add-src')
 var browserSync = require('browser-sync')
 var del = require('del')
+var runSequence = require('run-sequence')
 
 var config = {
   paths: {
@@ -26,39 +27,50 @@ var config = {
   }
 }
 
+// Returns a webpack pipeline that builds a bundle based on the specified webpack config
+function pipelineForWebpackConfig(webpackConfig) {
+  return gulp.src(config.paths.entryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(addsrc.append(config.paths.indexHtml))
+    .pipe(gulp.dest(config.paths.outputDir))
+}
+
+// Delete the build folder
 gulp.task('clean', function(cb) {
   del(['build'], cb)
 })
 
+// Copy static components files to the build folder under components/
 gulp.task('copy-components', function() {
   return gulp.src(config.paths.components.src)
     .pipe(gulp.dest(config.paths.components.dest))
 })
 
-// Don't clean while the server is running, it causes crashes
+// Dirty build: Don't clean while the server is running, it causes crashes
 gulp.task('build-dev-dirty', ['copy-components'], function() {
-  return gulp.src(config.paths.entryPoint)
-    .pipe(webpack(config.webpack.dev))
-    .pipe(addsrc.append(config.paths.indexHtml))
-    .pipe(gulp.dest(config.paths.outputDir))
+  return pipelineForWebpackConfig(config.webpack.dev)
 })
 
-// But clean on the first build
+// Produce a clean development build
 gulp.task('build-dev', ['clean', 'build-dev-dirty'])
 
+// Produce a clean production build
 gulp.task('build-prod', ['clean'], function() {
-  return gulp.src(config.paths.entryPoint)
-    .pipe(webpack(config.webpack.prod))
-    .pipe(addsrc.append(config.paths.indexHtml))
-    .pipe(gulp.dest(config.paths.outputDir))
+  return pipelineForWebpackConfig(config.webpack.prod)
 })
 
-gulp.task('server', ['build-dev'], function() {
+// Serve the dev build with BrowserSync
+gulp.task('server', ['build-dev-dirty'], function() {
   browserSync(config.browserSync)
 })
 
+// Watch the app files and serves them
 gulp.task('watch', ['server'], function() {
   gulp.watch(config.paths.watch, ['build-dev-dirty', browserSync.reload])
 })
 
-gulp.task('default', ['clean', 'watch'])
+// Ensure clean runs and completes before starting watcher.
+// Otherwise, copy-components can cause a race condition.
+gulp.task('default', function(callback) {
+  runSequence('clean', 'watch', callback)
+})
